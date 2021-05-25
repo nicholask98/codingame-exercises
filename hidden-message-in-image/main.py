b79011a4a673cc34bae2669b8eeef290aabb9170
nicholask98 <72052647+nicholask98@users.noreply.github.com>
2021-05-24 18:09:51 -0700

    dfghj

---
+++ hidden-message-in-image/main.py
@@ -0,0 +1,14 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+w, h = [int(i) for i in input().split()]
+for i in range(h):
+    for j in input().split():
+        pixel = int(j)
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
